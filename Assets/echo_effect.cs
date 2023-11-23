ef3f44a0119061d33ff2b5462a87aac1696fe90c
Lucas Camarino Emerick de Almeida <104381447+lunatos@users.noreply.github.com>
2023-11-23 01:21:18 -0300

    cutScene, visual effect
    cutscenes e particulas nos buracos

--- Assets/echo_effect.cs
+++ Assets/echo_effect.cs
@@ -13,7 +13,7 @@ public class echo_effect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Player>();
     }
 
     // Update is called once per frame
